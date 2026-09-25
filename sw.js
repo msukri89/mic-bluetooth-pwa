d7434254e2ce7cfc058c34239f1788f885358b09
msukri89 <mochammadsyukri@gmail.com>
2026-09-25 13:58:09 +0700

    Add Mic Bluetooth PWA v1

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="mic-bluetooth-v1";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
